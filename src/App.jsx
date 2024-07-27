5a4876b33471ced396157209801c96b83ac8f6af
Richard Suarez <richardmsc24@gmail.com>
2024-07-27 19:20:58 +0200

    addToCart added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import { Home } from "./pages/Home";
 import { ProductDetails } from "./pages/ProductDetails";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { CartProvider } from "./components/CartContext";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
       </Routes>
-    </>
+    </CartProvider>
   );
 }
 
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 import { useProducts } from "../useProducts";
 import "../styles/productDetail.css";
 import { Rate } from "../components/Rate";
+import useCart from "../components/useCart";
 
 export function ProductDetails() {
   const { productId } = useParams();
   const { products, error, isLoading } = useProducts();
+  const { addToCart } = useCart();
   const product = products.find((p) => p.id == productId);
 
   function capitalWords(str) {
@@ -18,13 +20,24 @@ export function ProductDetails() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading product details</div>;
   if (!product) return <div>Product not found</div>;
+
+  const handleAddToCart = () => {
+    addToCart(product);
+  };
+
   return (
     <>
       <section className="product-detail flex flex-row gap-12 my-12">
-        <div className="product-img-container w-1/3">
-          <img className="product-img" src={product.image} alt="" />
+        <div className="product-img-container w-1/3 h-[520px]">
+          <img
+            className="product-img hover:product-img-hover"
+            src={product.image}
+            alt=""
+          />
+          {/*<div className="zoomer" id="zoomer"></div>*/}
         </div>
         <div className="w-2/3 flex flex-col gap-4 text-start">
+          <div className="zoomCaptured "></div>
           <span className="rounded-full bg-orange-500 px-4 py-2 w-fit text-white text-sm font-semibold">
             {capitalWords(
               product.category === "jewelery" ? "jewelry" : product.category
@@ -38,6 +51,12 @@ export function ProductDetails() {
             <Rate rating={product.rating.rate} />
           </div>
           <p className="text-xl">{product.description}</p>
+          <button
+            onClick={handleAddToCart}
+            className="bg-blue-500 text-white px-4 py-2 rounded-full mt-4"
+          >
+            Add to Cart
+          </button>
         </div>
       </section>
     </>
