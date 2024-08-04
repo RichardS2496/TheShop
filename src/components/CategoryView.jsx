77ef223fdbd9dba04c5a7aad81db635bc1e29e78
Richard Suarez <richardmsc24@gmail.com>
2024-08-04 23:18:41 +0200

    giving some styles to filter section

--- src/components/CategoryView.jsx
+++ src/components/CategoryView.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { ProductView } from "../ProductView";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../styles/categoryView.css";
 
 export function CategoryView() {
   const { productCategory } = useParams();
@@ -11,20 +12,52 @@ export function CategoryView() {
     fetch(`https://fakestoreapi.com/products/category/${productCategory}`)
       .then((res) => res.json())
       .then((json) => setProducts(json));
-    console.log(productCategory);
   }, [productCategory]);
 
   return (
-    <section className="mt-4">
-      <ul className="grid grid-cols-5 gap-8 mb-16">
-        {products.map((product) => (
-          <li key={product.id}>
-            <Link to={`/product/${product.id}`}>
-              <ProductView product={product} />
+    <section className="category-section">
+      <div className="flex flex-row gap-8">
+        <div className="w-1/5 filter-section">
+          <nav className="grid grid-cols-1 gap-4  ">
+            <Link to="/" className="categoryFilter-card">
+              Home
             </Link>
-          </li>
-        ))}
-      </ul>
+
+            <Link className="categoryFilter-card">
+              <h4>Best Deals</h4>
+            </Link>
+            <Link
+              to={"/category/men's clothing"}
+              className="categoryFilter-card"
+            >
+              <h4>Men's Clothing</h4>
+            </Link>
+            <Link
+              to={"/category/women's clothing"}
+              className="categoryFilter-card"
+            >
+              <h4>Women's Clothing</h4>
+            </Link>
+            <Link to={"/category/electronics"} className="categoryFilter-card">
+              <h4>Electronics</h4>
+            </Link>
+            <Link to={"/category/jewelery"} className="categoryFilter-card">
+              <h4>Jewelry</h4>
+            </Link>
+          </nav>
+        </div>
+        <div className="w-4/5 ">
+          <ul className="grid grid-cols-4 gap-8 mb-16">
+            {products.map((product) => (
+              <li key={product.id}>
+                <Link to={`/product/${product.id}`}>
+                  <ProductView product={product} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
