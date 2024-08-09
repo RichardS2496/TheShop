b3bd708fdc7ef43c14b1163da338092071addb86
Richard Suarez <richardmsc24@gmail.com>
2024-08-09 20:11:09 +0200

    Configured environment variables

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdykNJjKUU7AsTiajZZ5f099TTo5jGHN8",
-  authDomain: "theshop-19b93.firebaseapp.com",
-  projectId: "theshop-19b93",
-  storageBucket: "theshop-19b93.appspot.com",
-  messagingSenderId: "889844373707",
-  appId: "1:889844373707:web:7ce18d73786dabadece34e",
-  measurementId: "G-PX5QGJ0DZX",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
